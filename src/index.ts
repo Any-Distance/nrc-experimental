da24cae26f5fa959e180fb564f7522e33923d03d
kylebragger <kyle.bragger@gmail.com>
2022-08-24 14:05:14 -0400

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -25,18 +25,19 @@ export default {
         env: Env,
         ctx: ExecutionContext
     ): Promise<Response> {
-        const after = 0; // TODO: pull from params
+        const after = 0; // TODO: pull from url params
         const endpoint = `https://api.nike.com/sport/v3/me/activities/after_time/${after}`;
+
+        // TODO: token should be POSTed; also, only respond to POST
         const token = 'eyJhbGciOiJSUzI1NiIsImtpZCI6ImVkZmY4YTAyLWIyZGEtNDhkZC1iOGVjLThhMmM1MGE2YTM0MnNpZyJ9.eyJpYXQiOjE2NjEzNjE0MjAsImV4cCI6MTY2MTM2NTAyMCwiaXNzIjoib2F1dGgyYWNjIiwianRpIjoiOGE1NzhkY2MtZjY1MS00MGQyLThiNzUtYWNkOTk0YmRhOWVlIiwiYXVkIjoiY29tLm5pa2UuZGlnaXRhbCIsInNidCI6Im5pa2U6YXBwIiwidHJ1c3QiOjEwMCwibGF0IjoxNjYxMzYxNDE5LCJzY3AiOlsibmlrZS5kaWdpdGFsIl0sInN1YiI6ImNvbS5uaWtlLmNvbW1lcmNlLm5pa2Vkb3Rjb20ud2ViIiwicHJuIjoiZjNkYzRlNmMtZmJmYy00YWRlLTgzNWEtYzBiYWFiMTA3MmQ0IiwicHJ0IjoibmlrZTpwbHVzIiwibHJzY3AiOiJvcGVuaWQgbmlrZS5kaWdpdGFsIHByb2ZpbGUgZW1haWwgcGhvbmUgZmxvdyBjb3VudHJ5In0.O8tH47ZCO-tIlODDD1iufEMDvIZYQLsQHbS4hPfiIdkuy160jxBgIcR0HI3UvZFJ5HpcC6BrJ-0UiDRwDWxAxkn7KhHiB5L9ZXvIvpE2CbpEgBjnj9JVKlu1btL1l89p0XNFWzNpffnOemenvZQywy3N5EWw2qLCGVltPlDxsypt1jIiXi287ootyWs2ITxFUVOZWZv6q2xoNH3k18r31-wsdrsekiW9dwn-mT9y1sDciCP95oHQZHRjQT9qXy7fCgVEnue1BHYOui7iWWLaMuW1mWzEMnIuMh58DjlDAKtMTlJ6K_MXNTQ-Pb1q6-85x3F8P6cWYfDYpQ-AEYhX1g';
         const reqHeaders = new Headers({
             'Authorization': `Bearer ${token}`
         });
 
+        // TODO: paging.after_id would be set if there are multiple pages; pulling activity w/ metrics should be a function, maybe
         let activityResp: any = await fetch(endpoint, {
             headers: reqHeaders
         }).then((resp) => resp.json());
-
-        // TODO: paging.after_id would be set if there are multiple pages
 
         const metricsResp: any = {};
         const metricsPromise = await Promise.all(activityResp['activities'].map((activity) => {
@@ -49,21 +50,6 @@ export default {
         for (const metric of metricsJson) {
             metricsResp[metric['id']] = metric;
         }
-
-
-        // for (const activity of activityResp['activities']) {
-        //     const metricsResps = await Promise.all(acti);
-
-
-
-        //     await fetch(`https://api.nike.com/sport/v3/me/activity/${activity['id']}?metrics=ALL`, {
-        //         headers: reqHeaders
-        //     }).then((resp) => {
-        //         resp.json()
-        //     }).then((data) => {
-        //         metricsResp[activity['id']] = data;
-        //     });
-        // }
 
         activityResp['metrics'] = metricsResp;
 
